86389bac0f22df4c68fce071bb008b43f9092ad7
Daniel Vianna <dmlvianna@gmail.com>
2019-04-03 12:10:26 +1100

    add taffybar battery icon

--- taffybar.hs
+++ taffybar.hs
@@ -76,7 +76,7 @@ main = do
         { startWidgets =
             workspaces : Prelude.map (>>= buildContentsBox) [ layout, windows ]
         , endWidgets = Prelude.map (>>= buildContentsBox)
-          [ textBatteryNew "$status$ $percentage$% ($time$)"
+          [ batteryIconNew -- textBatteryNew "$status$ $percentage$% ($time$)"
           , clock
           , tray
           , cpu
